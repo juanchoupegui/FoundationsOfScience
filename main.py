5396fc29dffe4a2498771ba3017e83160aa9312c
juanchoupegui <jotapeacheupe@hotmail.com>
2024-01-02 15:33:00 +0100

    Input number if/else, elif added

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ number = int(input("Enter a number: "))
 
 if number > 5:
     print("Number is larger than 5")
+elif number == 5:
+    print("Number is equal to 5")
 else:
-    print("Number is equal or smaller than 5")
+    print("Number is smaller than 5")
